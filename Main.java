985c20b5f4ed0fdba8faebece2dac813f9adb564
Alex Kaz <you@example.com>
2025-10-20 16:09:06 +0700

    Добавлен метод getCarByBrendAndYearOperational

--- Main.java
+++ Main.java
@@ -6,9 +6,11 @@ public class Main {
             new Car(3, "Toyota", "Camry", 2022, "Black", 30000, "EF9012")
         };
 
-        Car[] toyotas = getCarByBrend(cars, "Toyota");
+        Car[] toyotas1 = getCarByBrend(cars, "Toyota");
+        System.out.println("Найдено Toyota: " + toyotas1.length);
 
-        System.out.println("Найдено машин Toyota: " + toyotas.length);
+        Car[] toyotas2 = getCarByBrendAndYearOperational(cars, "Toyota", 4);
+        System.out.println("Найдено Toyota более 4 лет: " + toyotas2.length);
     }
 
     public static Car[] getCarByBrend(Car[] cars, String mark) {
@@ -21,6 +23,32 @@ public class Main {
         }
 
         if (count==0) {
+            return new Car[0];  // Если под условие, то [0]
+        }
+
+        // Новый массив для результата
+        Car[] res = new Car[count];
+        int index = 0;
+
+        for (int i = 0; i <= cars.length-1; i++) {
+            if (cars[i].getMark().equals(mark)) {
+                res[index++] = cars[i];
+            }
+        }
+
+        return res;
+    }
+
+    public static Car[] getCarByBrendAndYearOperational(Car[] cars, String mark, int years) {
+        int count = 0;
+
+        for (int i = 0; i <= cars.length-1; i++) {
+            if (cars[i].getMark().equals(mark) && 2025-cars[i].getYear() >= years) {
+                count++;
+            }
+        }
+
+        if (count==0) {
             return new Car[0];
         }
 
@@ -28,7 +56,7 @@ public class Main {
         int index = 0;
 
         for (int i = 0; i <= cars.length-1; i++) {
-            if (cars[i].getMark().equals(mark)) {
+            if (cars[i].getMark().equals(mark) && 2025-cars[i].getYear() >= years) {
                 res[index++] = cars[i];
             }
         }
