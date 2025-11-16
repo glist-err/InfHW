58232ff36767dfde8a1f6b15ad49aec6680973f7
Alex Kaz <you@example.com>
2025-11-16 11:37:16 +0700

    реализован внутренний класс FileCacheEntry

--- ClassicIOCacheWithLimit.java
+++ ClassicIOCacheWithLimit.java
@@ -1,10 +1,18 @@
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.util.HashMap;
 import java.util.Map;
 
 public class ClassicIOCacheWithLimit {
     private Map<String, FileCacheEntry> cache;
     private int maxSize;
+
+    public static class FileCacheEntry {
+        String content;
+        long lastReadTime;
+        long lastModifiredTimeRead;
+
+        public FileCacheEntry(String content, long lastReadTime, long lastModifiredTimeRead) {
+            this.content = content;
+            this.lastReadTime = lastReadTime;
+            this.lastModifiredTimeRead = lastModifiredTimeRead;
+        }
+    }
 }
