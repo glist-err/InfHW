d19fde89783d50f3a966e3bc6385634f22b22ff3
Alex Kaz <you@example.com>
2025-11-16 11:53:49 +0700

    реализован метод readFile()

--- ClassicIOCacheWithLimit.java
+++ ClassicIOCacheWithLimit.java
@@ -1,9 +1,17 @@
+import java.awt.image.CropImageFilter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.HashMap;
 import java.util.Map;
+
+import com.sun.jdi.AbsentInformationException;
 
 public class ClassicIOCacheWithLimit {
     private Map<String, FileCacheEntry> cache;
     private int maxSize;
 
+    // внутренний класс
     public static class FileCacheEntry {
         String content;
         long lastReadTime;
@@ -13,6 +21,39 @@ public class ClassicIOCacheWithLimit {
             this.content = content;
             this.lastReadTime = lastReadTime;
             this.lastModifiredTimeRead = lastModifiredTimeRead;
+        }
+    }
+
+    // Конструкторы
+    public ClassicIOCacheWithLimit(int maxSize) {
+        this.cache = new HashMap<>();
+        this.maxSize = maxSize;
+    }
+
+    public ClassicIOCacheWithLimit() {
+        this.maxSize = 100;
+        this.cache = new HashMap<>();
+        //this.cache = new ClassicIOCacheWithLimit(maxSize);
+    }
+    // Методы
+    public String readFile(String filePath) throws IOException {
+        File file = new File(filePath);
+
+        if (!file.exists()) {
+            throw new FileNotFoundException("Нет файла " + filePath);
+        }
+
+        // путь и время файла
+        String absPath = file.getAbsolutePath();
+        long curModifiredTime = file.lastModified();
+        // есть ли файл в кеше
+        FileCacheEntry cached = cache.get(absPath);
+        
+        if (cached != null && isCacheValid(cached, curModifiredTime)) {
+            return cached.content;
+        }
+        else {
+            return updateCache(file, absPath, curModifiredTime);
         }
     }
 }
